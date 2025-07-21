b824e31733605886e563f1d989ce279515cf430b
Alex <bolonin.lex@yandex.ru>
2025-07-21 21:45:53 +0300

    Update config.js

--- src/api/firebase/config/config.js
+++ src/api/firebase/config/config.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCINbJWdqg-dnsnBL_ge2GRKe_Fhn3AghY",
-  authDomain: "notes-40f85.firebaseapp.com",
-  projectId: "notes-40f85",
-  storageBucket: "notes-40f85.firebasestorage.app",
-  messagingSenderId: "716593236948",
-  appId: "1:716593236948:web:30ab2ba8a032548e3ee441",
+  // Your firebase api config
 };
 
 export default firebaseConfig;
